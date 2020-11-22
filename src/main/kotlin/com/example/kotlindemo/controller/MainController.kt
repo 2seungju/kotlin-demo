6bd762cbf2cfed924da3751477ea10f9c6a705c4
unknown <a01065253918@gmail.com>
2020-11-23 02:00:13 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlindemo/controller/MainController.kt
@@ -0,0 +1,15 @@
+package com.example.kotlindemo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api")
+class MainController {
+
+    @GetMapping("hello-world")
+    fun helloWorld(): String {
+        return "Hello World!";
+    }
+}
